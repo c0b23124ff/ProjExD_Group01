463cf152f62151d0cf9f709a7ffda8256f8a2b18
c0b23092 <c0b23092db@edu.teu.ac.jp>
2024-05-21 18:25:52 +0900

    C0B23092:強化欄の作成_機能実装_画像付ける？

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import math
 import random
 import time
 
-WIDTH, HEIGHT = 1280,720
+WIDTH,HEIGHT = 1280,720
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+FRAMERATE = 60
 
 class Player(pg.sprite.Sprite):
     '''プレイヤー'''
@@ -16,17 +17,61 @@ class Player(pg.sprite.Sprite):
         pass
 
 class Reinforcement(pg.sprite.Sprite):
-    '''強化欄'''
-    def __init__(self,) -> None:
-        pass
-    def dummy(self):
-        pass
-    def update(self):
-        pass
+    '''
+    total_output -> totalを入れる
+    timer_clock -> フレーム
+    total_input -> 
+    '''
+    def __init__(self,x,y,total_output,timer_clock,total_input) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.total_output = total_output
+        self.timer_clock = timer_clock
+        self.total_input = total_input
+        self.object_number = 0
+        self.font = pg.font.Font(None,10)
+        # input
+        self.text_input = self.font.render(str(""), True, (255, 255, 255))
+        self.rect_text_input = self.text_input.get_rect()
+        self.rect_text_input.center = x-150,y+20
+        # output
+        self.text_output = self.font.render(str(""), True, (255, 255, 255))
+        self.rect_text_output = self.text_output.get_rect()
+        self.rect_text_output.center = x-150,y-20
+        # count
+        self.text_counter = self.font.render(str(""), True, (255, 255, 255))
+        self.rect_text_counter = self.text_counter.get_rect()
+        self.rect_text_counter.center = x+50,y-20
+        self.image = pg.Surface((WIDTH/2-200,100))
+        pg.draw.rect(self.image,(0,0,0),(WIDTH/2-200,0,WIDTH/2+200,100))
+        self.rect = self.image.get_rect()
+        self.rect.center = x,y
 
-class Total():
+    def check(self, pos):
+        return self.rect.collidepoint(pos)
+
+    def counter(self,total):
+        if total >= self.total_output:
+            self.object_number += 1
+            return self.total_output
+        else:
+            return 0
+
+    def update(self,screen:pg.Surface):
+        self.font = pg.font.Font(None,50)
+        self.text_output = self.font.render(str(self.total_output),True,(255, 255, 255))
+        self.text_input = self.font.render(str(self.timer_clock),True,(255, 255, 255))
+        self.font = pg.font.Font(None,100)
+        self.text_counter = self.font.render(str(self.object_number),True,(255, 255, 255))
+        screen.blit(self.image,self.rect)
+        screen.blit(self.text_counter,self.rect_text_counter)
+        screen.blit(self.text_input,self.rect_text_input)
+        screen.blit(self.text_output,self.rect_text_output)
+
+class Total:
     '''現在の総数'''
-    def __init__(self) -> None:
+    def __init__(self):
         self.value = 0
     def dummy(self):
         pass
@@ -37,9 +82,12 @@ def main():
     screen = pg.display.set_mode((WIDTH,HEIGHT))
     bg_img = pg.image.load(f"image/dummy_0.png")
     total = Total()
-    total.value = 10000
+    total.value = int(10000)
     player = Player()
-    r_block = Reinforcement()
+    reinforcement_list = [[5,FRAMERATE,1],[20,FRAMERATE/2,1],[100,FRAMERATE/6,1],[1000,1,1]]
+    r_blocks = pg.sprite.Group()
+    for y,(q,w,e) in zip(range(len(reinforcement_list)),reinforcement_list):
+        r_blocks.add(Reinforcement(WIDTH/4*3,100+y*125,q,w,e))
     timer = 0
     clock = pg.time.Clock()
 
@@ -49,21 +97,36 @@ def main():
             if event.type == pg.QUIT:
                 return 0
             if event.type == pg.KEYDOWN:
-
                 if event.key == pg.K_z:
                     total.value -= 1
-                    print(total.value)
                 if event.key == pg.K_x:
                     total.value += 1
-                    print(total.value)
+                print(total.value)
+            if event.type == pg.MOUSEBUTTONDOWN:
+                x,y = event.pos
+                for r_block in r_blocks:
+                    if r_block.check(event.pos):
+                        total.value -= r_block.counter(total.value)
+                        print(total.value,r_block.y)
+                print(f"mouse moved -> ({x},{y})")
         screen.blit(bg_img,[0,0])
 
+
+
+
         # player.update(screen)
-        # r_block.update(screen)
+        total_sum = 0
+        for r_block in r_blocks:
+            if timer % r_block.timer_clock == 0:
+                total_sum += r_block.total_input * r_block.object_number
+        total.value += total_sum
+        r_blocks.draw(screen)
+        r_blocks.update(screen)
         # total.update(screen)
         pg.display.update()
         timer += 1
-        clock.tick(60)
+        clock.tick(FRAMERATE)
+        print(total.value)
 
 if __name__ == "__main__":
     pg.init()
